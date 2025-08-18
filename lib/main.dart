f6ad06af201774b0a0bb460b8f23536eddab6c97
sb-dor <avaz.shams.2002@gmail.com>
2025-08-18 06:11:08 +0500

    learning about keys

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,8 @@ import 'package:yandex_flutter_handbook/test_widget_keys.dart';
 import 'test_widget_keys_2.dart';
 import 'test_widget_keys_3.dart';
 import 'test_widget_keys_4.dart';
+import 'test_widget_keys_5.dart';
 
 void main() {
-  runApp(MaterialApp(home: TestWidgetKeys4()));
+  runApp(MaterialApp(home: TestWidgetKeys5()));
 }
---
+++ lib/test_widget_keys_5.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class TestWidgetKeys5 extends StatefulWidget {
+  const TestWidgetKeys5({super.key});
+
+  @override
+  State<TestWidgetKeys5> createState() => _TestWidgetKeys5State();
+}
+
+class _TestWidgetKeys5State extends State<TestWidgetKeys5> {
+  List<Widget> _children = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _children = [
+      _TestDidUpdateWidgetNoKeys(
+        color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+      ),
+      _TestDidUpdateWidgetNoKeys(
+        color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+      ),
+      _TestDidUpdateWidgetNoKeys(
+        color: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Test widget keys 5")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            final copiedList = List.of(_children)..shuffle();
+            _children = copiedList;
+          });
+        },
+        child: Icon(Icons.change_circle),
+      ),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [SliverToBoxAdapter(child: Row(children: _children))],
+        ),
+      ),
+    );
+  }
+}
+
+class _TestDidUpdateWidgetNoKeys extends StatefulWidget {
+  const _TestDidUpdateWidgetNoKeys({super.key, required this.color});
+
+  final Color color;
+
+  @override
+  State<_TestDidUpdateWidgetNoKeys> createState() => _TestDidUpdateWidgetNoKeysState();
+}
+
+class _TestDidUpdateWidgetNoKeysState extends State<_TestDidUpdateWidgetNoKeys> {
+  Color? _color;
+
+  @override
+  void initState() {
+    super.initState();
+    _color = widget.color;
+  }
+
+  @override
+  void didUpdateWidget(covariant _TestDidUpdateWidgetNoKeys oldWidget) {
+    _color = widget.color;
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: 100, height: 100, color: _color);
+  }
+}
