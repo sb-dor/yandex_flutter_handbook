433c5b50c1af0c57470eaa93b62c6f4967afad7f
sb-dor <avaz.shams.2002@gmail.com>
2025-08-21 21:33:29 +0500

    remembering oop

---
+++ lib/remember_oop_2.dart
@@ -0,0 +1,46 @@
+void main() {
+  final assortment = Assortment([Product(10), Product(12)]);
+  final product = Product(25);
+
+  final cart = Cart(cartItems: [CartItem(assortment, 2), CartItem(product, 1)]);
+  print(cart.total);
+}
+
+sealed class ProductInh {
+  double get price;
+}
+
+class Product extends ProductInh {
+  Product(this._price);
+
+  final double _price;
+
+  @override
+  double get price => _price;
+}
+
+class Assortment extends ProductInh {
+  Assortment(this._products);
+
+  final List<Product> _products;
+
+  @override
+  double get price => _products.fold(0.0, (sum, prod) => sum += prod.price);
+}
+
+class Cart {
+  const Cart({this.cartItems = const []});
+
+  final List<CartItem> cartItems;
+
+  double get total => cartItems.fold(0.0, (sum, item) => sum += item.total);
+}
+
+class CartItem {
+  CartItem(this.cartItem, this.qty);
+
+  final ProductInh cartItem;
+  final double qty;
+
+  double get total => cartItem.price * qty;
+}
