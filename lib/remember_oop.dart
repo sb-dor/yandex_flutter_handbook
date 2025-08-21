b24e394f6cf61762894ea1ca3c43e9b62a2dd930
sb-dor <avaz.shams.2002@gmail.com>
2025-08-21 21:24:28 +0500

    test oop class

---
+++ lib/remember_oop.dart
@@ -0,0 +1,59 @@
+void main() {
+  final test = Test<TestClass>(Second());
+    switch(test.test){
+      case First(): print("firstg");
+        break;
+      case Second(): print("second");
+        break;
+    }
+}
+
+sealed class TestClass {
+  void testMethod();
+}
+
+final class First extends TestClass {
+  void testMethod() {
+    print("first");
+  }
+}
+
+final class Second extends TestClass {
+  void testMethod() {
+    print("second");
+  }
+}
+
+class Test<G> {
+  Test(this.test);
+
+  final G test;
+}
+
+mixin class Test1FunctionOverall {
+  void test1() {}
+}
+
+abstract interface class TestInterface {
+  void test1();
+
+  void test2() {}
+}
+
+class TestInterfaceImpl with Test1FunctionOverall implements TestInterface {
+  void test2() {}
+}
+
+class MyAbstractMixin {
+  void bar() {}
+}
+
+class JustClassTestClass implements MyAbstractMixin {
+  void testMethod() {
+    print("JustClassTestClass");
+  }
+
+  void bar() {
+    print("JustClassTestClass");
+  }
+}
