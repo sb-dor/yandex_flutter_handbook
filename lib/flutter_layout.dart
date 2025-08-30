2901c592998c43fce27db3a6e294ecaa7398a591
sb-dor <avaz.shams.2002@gmail.com>
2025-08-30 17:08:35 +0500

    layout in flutter

---
+++ lib/flutter_layout.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: FlutterLayout()));
+
+class FlutterLayout extends StatefulWidget {
+  const FlutterLayout({super.key});
+
+  @override
+  State<FlutterLayout> createState() => _FlutterLayoutState();
+}
+
+class _FlutterLayoutState extends State<FlutterLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Test5();
+  }
+}
+
+class _Test1 extends StatelessWidget {
+  const _Test1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: 100, height: 100, color: Colors.red);
+  }
+}
+
+class Test2 extends StatelessWidget {
+  const Test2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Container(width: 100, height: 100, color: Colors.red));
+  }
+}
+
+class Test3 extends StatelessWidget {
+  const Test3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 100,
+        height: 100,
+        padding: const EdgeInsets.all(20),
+        color: Colors.red,
+        child: Row(children: [Container(width: 50, height: 50, color: Colors.green)]),
+      ),
+    );
+  }
+}
+
+class Test4 extends StatelessWidget {
+  const Test4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(width: 100, height: 100, color: Colors.red),
+        SizedBox(height: 10),
+        Container(
+          color: Colors.yellow,
+          child: Row(
+            children: [
+              Container(width: 20, height: 20, color: Colors.green),
+              Container(height: 30, width: 30, color: Colors.orange),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Test5 extends StatelessWidget {
+  const Test5({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(width: 100, height: 100, color: Colors.red),
+        SizedBox(width: 10),
+        Container(
+          color: Colors.yellow,
+          child: Column(
+            children: [
+              Container(width: 20, height: 20, color: Colors.green),
+              Container(height: 30, width: 30, color: Colors.orange),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
