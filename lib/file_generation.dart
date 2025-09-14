249b87ad80206787ea7a5b5e1920124e7bfc88d2
sb-dor <avaz.shams.2002@gmail.com>
2025-09-14 21:45:08 +0500

    learning about files

---
+++ lib/file_generation.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await deleteFolder();
+}
+
+Future<void> fileGeneration() async {
+  Directory? downloadsProvider = await getDownloadsDirectory();
+  final doesDownloadFolderExits = await downloadsProvider?.exists();
+  if (!(doesDownloadFolderExits ?? false)) {
+    downloadsProvider = await downloadsProvider?.create();
+  }
+  // for macos open Runner and give permission for downloads folder
+  for (int i = 0; i < 10; i++) {
+    final file = File("${downloadsProvider?.path}/tempfiles/text$i.txt");
+    final doesFileExits = await file.exists();
+    if (!doesFileExits) await file.create(recursive: true);
+    await file.writeAsString("Hello, world!");
+  }
+}
+
+Future<void> deleteFile() async {
+  Directory? downloadsProvider = await getDownloadsDirectory();
+  final file = File("${downloadsProvider?.path}/tempfiles/text0.txt");
+  final doesFileExits = await file.exists();
+  if (doesFileExits) {
+    await file.delete();
+  }
+}
+
+Future<void> deleteFolder() async {
+  Directory? downloadsProvider = await getDownloadsDirectory();
+  final file = Directory("${downloadsProvider?.path}/tempfiles");
+  final doesFileExits = await file.exists();
+  if (doesFileExits) {
+    await file.delete(recursive: true);
+  }
+}
