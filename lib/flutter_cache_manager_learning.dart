0b30fec86604cb601f12a13bdc47f238ec0ccc00
sb-dor <avaz.shams.2002@gmail.com>
2025-09-16 06:56:30 +0500

    cache manager

--- lib/flutter_cache_manager_learning.dart
+++ lib/flutter_cache_manager_learning.dart
@@ -3,13 +3,36 @@ import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await getSingleFile();
+  await ownFileManager();
 }
 
+// but exactly it's good to use default Cache manager
 Future<void> getSingleFile() async {
-  print("starting do getting file");
+  print("starting to getting image file");
   final file = await DefaultCacheManager().getSingleFile(
     'https://yastatic.net/s3/ml-handbook/admin/edu_logo_0640b6dbf8.svg',
   );
   print(file.path);
+}
+
+Future<void> ownFileManager() async {
+  print("starting to getting own cachemanager image file");
+  final file = await CustomCacheManager.instance.getSingleFile(
+    'https://yastatic.net/s3/ml-handbook/admin/edu_logo_0640b6dbf8.svg',
+  );
+  print(file.path);
+}
+
+class CustomCacheManager {
+  static const _key = 'customCacheKey';
+  static CacheManager instance = CacheManager(
+    Config(
+      _key,
+      stalePeriod: const Duration(days: 7),
+      maxNrOfCacheObjects: 20,
+      repo: JsonCacheInfoRepository(databaseName: _key),
+      fileSystem: IOFileSystem(_key),
+      fileService: HttpFileService(),
+    ),
+  );
 }
